083f52c60c4486f4432c2cd8ccceaa1a6ae2726a
Leon2323232323232323232323 <2224634039@qq.com>
2024-07-12 01:52:29 -0400

    Edit

--- src/Blog2.vue
+++ src/Blog2.vue
@@ -3,42 +3,25 @@
   <div style="width:1240px;padding:40px;margin:auto;">
     <div class="grid-container">
       <div class="abovehead" style="padding:0px">
-        <div style="text-align:left;font-size:16px;font-family:cursive">Your Name</div>
-        <div style="text-align:right;font-size:16px;position:relative;top:-30px;right:50px">
-          <ul class="pagination">
-            <li><a href="#">Blog</a></li>
-            <li><a href="#">Projects</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Newsletter</a></li>
-          </ul>
-        </div>
         <!-- Change Color-->
-        <div class="changecolor" style="padding:0px">
-          <div style="background-color:black;position:absolute;top:59px;right:-628px;height:20px;width:50px;border-radius:100px;z-inte">
-            <div style="height: 17px; width:17px;position:relative;top:1.5px;right:-31px;background-color:white;border-radius:100%;"></div>
-            <div style="height: 17px; width:17px;position:absolute;top:0.5px;left:1px;border-style:dotted;border-radius:100%;border-color:white"></div>
-            <div style="height: 10px; width:10px;position:absolute;top:4px;left:4px;border-style:solid;border-radius:100%;border-color:white"></div>
-
-          </div>
-
-
-        </div>
+        
+     
 
 
       </div>
       <div class="item1" style="font-family:fantasy;font-size:200px;border-top:solid gray;border-bottom:solid gray">THE BLOG</div>
-      <div class="transition" style="font-size:20px;text-align:left;padding-top:20px;padding-bottom:20px">Recent Blog Posts</div>
+      <div class="transition" style="font-size:20px;text-align:left;padding-top:20px;padding-bottom:20px"><h3>Recent Blog Posts</h3></div>
       <!-- Left Box -->
       <div class="tleft" style="padding:0px; width:600px;height:440px">
         <div style="width:600px;height:220px;overflow:hidden;">
           <img src="../public/Picture1.jfif" alt="First" style="max-width:600px;height:auto;position:relative;top:-100px">
         </div>
         <p style="color:purple;font-size:14px;text-align:left;padding:0px">Olivia Rhye • 1 Jan 2023</p>
-        <h5 style="text-align:left;padding:0px;position:relative; top:-30px;">UX Review Presentations
+        <h5><a href="./posts/example-post" style="text-align:left;padding:0px;position:relative;top:-30px;left:-160px;color:black;text-decoration-line:none">UX Review Presentations</a>
           <img src="../public/Icon.png" alt="Icon" style="position:relative;right:-300px;">
         </h5>
         <p style="text-align:left;font-size:18px;position:relative;top:-50px">How do you create compelling presentations that wow your colleagues and impress your managers?</p>
-        <div style="text-align:right;font-size:16px;position:relative;left:-362px;top:-50px">
+        <div style="text-align:right;font-size:16px;position:relative;left:-375px;top:-60px">
           <ul class="pagination">
             <li style="padding:5px;margin:3px;background-color:rgba(225, 176, 225, 0.333);color:#6941C6;border-radius:100px">Design</li>
             <li style="padding:5px;margin:3px;background-color:rgb(214, 214, 246);color:#3538CD;border-radius:100px">Research</li>
@@ -50,11 +33,11 @@
       <div class="trightimage" style="width:300px;height:210px">
         <img src="../public/Picture2.jfif" alt="Second" style="max-width:260px;height:auto;">
       </div>
-      <div class="tright" style="width:300px;height:210px">
-        <p style="color:purple;font-size:14px;text-align:left;padding:0px">Phoenix Baker • 1 Jan 2023</p>
-        <h5 style="text-align:left;padding:0px;position:relative; top:-30px;">Migrating to Linear 101</h5>
-        <p style="text-align:left;font-size:18px;position:relative;top:-50px">Linear helps streamline software projects, sprints, tasks, and bug tracking. Here’s how to get...</p>
-        <div style="text-align:right;font-size:16px;position:relative;left:-160px;top:-60px">
+      <div class="tright" style="width:300px;height:210px;position:relative;top:-20px">
+        <p style="color:purple;font-size:14px;text-align:left;padding:0px;position:relative;">Phoenix Baker • 1 Jan 2023</p>
+        <h5> <a href="./posts/example-post" style="text-align:left;padding:0px;position:relative; top:-30px;left:-32px;color:black;text-decoration-line:none;font-family">Migrating to Linear 101</a></h5>
+        <p style="text-align:left;font-size:18px;position:relative;top:-60px;">Linear helps streamline software projects, sprints, tasks, and bug tracking. Here’s how to get...</p>
+        <div style="text-align:right;font-size:16px;position:relative;left:-170px;top:-80px">
           <ul class="pagination">
             <li style="padding:5px;margin:3px;background-color:rgba(176, 225, 214, 0.333);color:#32be55;border-radius:100px">Design</li>
             <li style="padding:5px;margin:3px;background-color:rgb(214, 214, 246);color:#3538CD;border-radius:100px">Research</li>
@@ -67,10 +50,10 @@
         <img src="../public/Picture3.jfif" alt="Third" style="max-width:260px;height:auto;">
       </div>
 
-      <div class="bright" style="height:229px">
+      <div class="bright" style="height:229px; position:relative; top:-20px">
         <p style="color:purple;font-size:14px;text-align:left;padding:0px">Lana Steiner • 1 Jan 2023</p>
-        <h5 style="text-align:left;padding:0px;position:relative; top:-30px;">Building your API Stack</h5>
-        <p style="text-align:left;font-size:18px;position:relative;top:-50px">The rise of RESTful APIs has been met by a rise in tools for creating, testing, and manag...</p>
+        <h5><a style="text-align:left;padding:0px;position:relative; top:-30px;left:-29px;color:black;text-decoration-line:none">Building your API Stack</a></h5>
+        <p href="./posts/example-post" style="text-align:left;font-size:18px;position:relative;top:-50px">The rise of RESTful APIs has been met by a rise in tools for creating, testing, and manag...</p>
         <div style="text-align:right;font-size:16px;position:relative;left:-240px;top:-60px">
           <ul class="pagination">
             <li style="padding:5px;margin:3px;background-color:rgba(176, 225, 214, 0.333);color:#32be55;border-radius:100px">Design</li>
@@ -81,7 +64,7 @@
       <!-- Bottom Box -->
       <div class="tbottom" style="height:300px">
         <p style="color:purple;font-size:14px;text-align:left;padding:0px;margin:0px; margin-left:20px">Olivia Rhye • 1 Jan 2023</p>
-        <h5 style="text-align:left;padding:0px;position:relative; top:-30px;margin-left:20px;">Grid system for better Design User Alibaba puhuitiface</h5>
+        <h5><a href="./posts/example-post" style="text-align:left;padding:0px;position:relative; top:-30px;left:-6px;color:black;text-decoration-line:none">Grid system for better Design User Alibaba puhuitiface</a></h5>
         <p style="text-align:left;font-size:18px;position:relative;top:-50px;margin-left:20px">A grid system is a design tool used to arrange content on a webpage. It is a series of vertical and horizontal lines that create a matrix of alibaba puhuitisecting points, which can be used to align and organize page elements. Grid systems are used to create a consistent look and feel across a website, and can help to make the layout more visually appealing and easier to navigate.</p>
         <div style="text-align:right;font-size:16px;position:relative;left:-376px;top:-60px">
           <ul class="pagination">
@@ -96,7 +79,7 @@
         </div>
       </div>
       <!-- Transition -->
-      <div class="transition2" style="font-size:20px;text-align:left;padding:2px;padding-left:10px;padding-bottom:20px;padding-top:20px">All Blog Posts</div>
+      <div class="transition2" style="font-size:20px;text-align:left;padding:2px;padding-left:10px;padding-bottom:20px;padding-top:30px"><h3>All Blog Posts</h3></div>
 
       <!-- All Blog Posts Box -->
       <!-- First Row -->
@@ -104,8 +87,8 @@
         <div style="width:400px;">
           <img src="../public/Picture5.jfif" alt="Picture5" style="max-width:380px;">
           <p style="color:purple;font-size:14px;text-align:left;padding:0px;margin:0px; margin-left:10px">Alec Whitten • 1 Jan 2023</p>
-          <h5 style="text-align:left;padding:0px;position:relative; top:-30px;margin-left:10px;">Bill Walsh leadership lessons
-            <img src="../public/Icon.png" alt="Icon" style="position:relative;top:5px;right:-48px">
+          <h5><a href="./posts/example-post" style="text-align:left;padding:0px;position:relative; top:-30px;left:-34px;color:black;text-decoration-line:none">Bill Walsh leadership lessons</a>
+            <img src="../public/Icon.png" alt="Icon" style="position:relative;top:-26px;right:-39px">
           </h5>
           <p style="text-align:left;font-size:18px;position:relative;top:-50px;margin-left:10px">Like to know the secrets of transforming a 2-14 team into a 3x Super Bowl winning Dynasty?</p>
           <div style="text-align:right;font-size:16px;position:relative;left:-202px;top:-60px">
@@ -118,9 +101,9 @@
         <div style="width:400px;">
           <div style="width:400px;">
             <img src="../public/Picture6.jfif" alt="Picture6" style="max-width:380px;">
-            <p style="color:purple;font-size:14px;text-align:left;padding:0px;margin:0px; margin-left:10px">Demi WIlkinson • 1 Jan 2023</p>
-            <h5 style="text-align:left;padding:0px;position:relative; top:-30px;margin-left:10px;">PM mental models
-              <img src="../public/Icon.png" alt="Icon" style="position:relative;top:5px;right:-154px">
+            <p style="color:purple;font-size:14px;text-align:left;padding:0px;margin:0px; margin-left:10px">Demi Wilkinson • 1 Jan 2023</p>
+            <h5><a href="./posts/example-post" style="text-align:left;padding:0px;position:relative; top:-30px;left:-85px;color:black;text-decoration-line:none">PM mental models</a>
+            <img src="../public/Icon.png" alt="Icon" style="position:relative;top:-26px;right:-87px">
             </h5>
             <p style="text-align:left;font-size:18px;position:relative;top:-50px;margin-left:10px">Mental models are simple expressions of complex processes or relationships.</p>
             <div style="text-align:right;font-size:16px;position:relative;left:-150px;top:-60px">
@@ -137,8 +120,8 @@
           <div style="width:400px;">
             <img src="../public/Picture7.jfif" alt="Picture7" style="max-width:380px;">
             <p style="color:purple;font-size:14px;text-align:left;padding:0px;margin:0px; margin-left:10px">Candice Wu • 1 Jan 2023</p>
-            <h5 style="text-align:left;padding:0px;position:relative; top:-30px;margin-left:10px;">What is Wireframing?
-              <img src="../public/Icon.png" alt="Icon" style="position:relative;top:5px;right:-115px">
+            <h5><a href="./posts/example-post" style="text-align:left;padding:0px;position:relative; top:-30px;left:-69px;color:black;text-decoration-line:none">What is Wireframing?</a>
+            <img src="../public/Icon.png" alt="Icon" style="position:relative;top:-26px;right:-75px">
             </h5>
             <p style="text-align:left;font-size:18px;position:relative;top:-50px;margin-left:10px">Introduction to Wireframing and its Principles. Learn from the best in the industry.</p>
             <div style="text-align:right;font-size:16px;position:relative;left:-253px;top:-60px">
@@ -155,11 +138,11 @@
         <div style="width:400px;">
           <img src="../public/Picture8.jfif" alt="Picture8" style="max-width:380px;">
           <p style="color:purple;font-size:14px;text-align:left;padding:0px;margin:0px; margin-left:10px">Natali Craig • 1 Jan 2023</p>
-          <h5 style="text-align:left;padding:0px;position:relative; top:-30px;margin-left:10px;">How collaboration makes us</h5>
-          <img src="../public/Icon.png" alt="Icon" style="position:relative;top:-98px;right:-150px">
-          <h5 style="text-align:left;padding:0px;position:relative; top:-78px;margin-left:-240px;display:inline">better designers</h5>
-          <p style="text-align:left;font-size:18px;position:relative;top:-50px;margin-left:10px">Collaboration can make our teams stronger, and our individual designs better.</p>
-          <div style="text-align:right;font-size:16px;position:relative;left:-253px;top:-60px">
+          <h5><a href="./posts/example-post" style="text-align:left;padding:0px;position:relative; top:-30px;left:-48px;color:black;text-decoration-line:none">How collaboration makes us</a></h5>
+          <img src="../public/Icon.png" alt="Icon" style="position:relative;top:-98px;right:-180px">
+          <h5><a href="./posts/example-post" style="text-align:left;padding:0px;position:relative; top:-153px;left:-111px;color:black;text-decoration-line:none">better designers</a></h5>
+          <p style="text-align:left;font-size:18px;position:relative;top:-170px;margin-left:10px">Collaboration can make our teams stronger, and our individual designs better.</p>
+          <div style="text-align:right;font-size:16px;position:relative;left:-258px;top:-180px">
             <ul class="pagination">
               <li style="padding:5px;margin:3px;background-color:rgba(225, 176, 225, 0.333);color:#6941C6;border-radius:100px">Design</li>
               <li style="padding:5px;margin:3px;background-color:rgb(241, 237, 248);color:#3538CD;border-radius:100px">Research</li>
@@ -170,11 +153,11 @@
           <div style="width:400px;">
             <img src="../public/Picture9.jfif" alt="Picture9" style="max-width:380px;">
             <p style="color:purple;font-size:14px;text-align:left;padding:0px;margin:0px; margin-left:10px">Drew Cano • 1 Jan 2023</p>
-            <h5 style="text-align:left;padding:0px;position:relative; top:-30px;margin-left:10px;">Our top 10 Javascript</h5>
-            <img src="../public/Icon.png" alt="Icon" style="position:relative;top:-98px;right:-175px">
-            <h5 style="text-align:left;padding:0px;position:relative; top:-78px;margin-left:-215px;display:inline">frameworks to use</h5>
-            <p style="text-align:left;font-size:18px;position:relative;top:-50px;margin-left:10px">JavaScript frameworks make development easy with extensive features and functionalities.</p>
-            <div style="text-align:right;font-size:16px;position:relative;left:-122px;top:-60px">
+            <h5><a href="./posts/example-post" style="text-align:left;padding:0px;position:relative; top:-30px;left:-83px;color:black;text-decoration-line:none">Our top 10 Javascript</a></h5>
+            <img src="../public/Icon.png" alt="Icon" style="position:relative;top:-98px;right:-182px">
+            <h5><a href="./posts/example-post" style="text-align:left;padding:0px;position:relative; top:-153px;left:-98px;color:black;text-decoration-line:none">frameworks to use</a></h5>
+            <p style="text-align:left;font-size:18px;position:relative;top:-170px;margin-left:10px">JavaScript frameworks make development easy with extensive features and functionalities.</p>
+            <div style="text-align:right;font-size:16px;position:relative;left:-131px;top:-180px">
               <ul class="pagination">
                 <li style="padding:5px;margin:3px;background-color:#e9fbf4;color:#027A48;border-radius:100px">Software Development</li>
                 <li style="padding:5px;margin:3px;background-color:#f7e8ef;color:#C11574;border-radius:100px">Tools</li>
@@ -188,12 +171,12 @@
           <div style="width:400px;">
             <img src="../public/Picture10.jfif" alt="Picture10" style="max-width:380px;">
             <p style="color:purple;font-size:14px;text-align:left;padding:0px;margin:0px; margin-left:10px">Orlando Diggs • 1 Jan 2023</p>
-            <h5 style="text-align:left;padding:0px;position:relative; top:-30px;margin-left:10px;">Podcast: Creating a better</h5>
+          <h5><a href="./posts/example-post" style="text-align:left;padding:0px;position:relative; top:-30px;left:-62px;color:black;text-decoration-line:none">Podcast: Creating a better</a></h5>
             <img src="../public/Icon.png" alt="Icon" style="position:relative;top:-98px;right:-150px">
-            <h5 style="text-align:left;padding:0px;position:relative; top:-78px;margin-left:-240px;display:inline">CX Community</h5>
+            <h5><a href="./posts/example-post" style="text-align:left;padding:0px;position:relative; top:-153px;left:-111px;color:black;text-decoration-line:none">CX Community</a></h5>
 
-            <p style="text-align:left;font-size:18px;position:relative;top:-50px;margin-left:10px">Starting a community doesn’t need to be complicated, but how do you get started?</p>
-            <div style="text-align:right;font-size:16px;position:relative;left:-192px;top:-60px">
+            <p style="text-align:left;font-size:18px;position:relative;top:-170px;margin-left:10px">Starting a community doesn’t need to be complicated, but how do you get started?</p>
+            <div style="text-align:right;font-size:16px;position:relative;left:-192px;top:-180px">
               <ul class="pagination">
                 <li style="padding:5px;margin:3px;background-color:rgba(225, 176, 225, 0.333);color:#6941C6;border-radius:100px">Podcast</li>
                 <li style="padding:5px;margin:3px;background-color:rgb(234, 232, 233);color:rgb(57, 56, 56);border-radius:100px">Customer Success</li>
@@ -228,12 +211,12 @@
       <div class="footer" style = "font-size:20px;text-align:left">
         <div style="text-align:left;font-size:20px;position:relative;left:-30px">
           <ul class="pagination">
-            <li><a href="#">© 2023</a></li>
-            <li><a href="#">Twitter</a></li>
-            <li><a href="#">LinkedIn</a></li>
-            <li><a href="#">Email</a></li>
-            <li><a href="#">RSS feed</a></li>
-            <li><a href="#">Add to Feedly</a></li>
+            <li><a href="./posts/example-post">© 2023</a></li>
+            <li><a href="./posts/example-post">Twitter</a></li>
+            <li><a href="./posts/example-post">LinkedIn</a></li>
+            <li><a href="./posts/example-post">Email</a></li>
+            <li><a href="./posts/example-post">RSS feed</a></li>
+            <li><a href="./posts/example-post">Add to Feedly</a></li>
 
 
           </ul>
