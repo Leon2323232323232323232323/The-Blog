f83ad264c4fb2c8f536b612e7cdecdb817f92346
xionkq <3030199850@qq.com>
2024-07-12 09:31:46 +0800

    chore: update

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/The-Blog/',
   title: "The Blog",
   description: "The Blog",
   themeConfig: {
